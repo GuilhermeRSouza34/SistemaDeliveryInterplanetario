08bba912ba4cb7c4cee78eb6aab8ce9e8d295348
Guilherme <guilhermersouza34@gmail.com>
2024-05-27 18:55:01 -0300

    Atualizando o App.js para conseguir rodar o codigo

--- delivery-app/src/App.js
+++ delivery-app/src/App.js
@@ -1,11 +1,20 @@
 // src/App.js
 import React from 'react';
 import CadastroEndereco from './components/CadastroEndereco';
+import ExibicaoEdicaoEndereco from './components/ExibicaoEdicaoEndereco';
 
 function App() {
+  const enderecoExemplo = {
+    nome: 'Exemplo',
+    lote: '1234',
+    cidade: 'CidadeExemplo',
+    pais: 'Marte',
+  };
+
   return (
     <div className="App">
       <CadastroEndereco />
+      <ExibicaoEdicaoEndereco enderecoInicial={enderecoExemplo} />
     </div>
   );
 }
